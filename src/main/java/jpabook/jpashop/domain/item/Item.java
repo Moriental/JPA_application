2d0e36329363319d6901d7444136a8d393c48e40
kim <youngwoo7804@gmail.com>
2025-01-19 00:18:46 +0900

    Item 엔티티에 핵심 로직 추가 (addStock,removeStock)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직 //
+    /**
+     * 재고 수량 증가하는 로직
+     * **/
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    } //setter를 가지고 변경하는 것 이 아닌 핵심 로직을 이용해서 변경
+    /**
+     * 재고 수량 감소하는 로직
+     * **/
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
